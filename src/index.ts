ff63ac96e08e6441c59c4812c0fe7b489abad7e9
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-19 21:29:55 -0300

    🎉 (feature): Begin a project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { settings } from "./settings";
+
+const APP_PORT = settings.APP_PORT;
+
+const app = express()
+
+app.use(express.json())
+
+app.get('', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(APP_PORT, () => {
+  console.log(`Server is running on port ${APP_PORT}`)
+})
