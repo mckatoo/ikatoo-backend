3da169548c7c8e988b5d7849335ef7f24f2f982d
Milton Carlos Katoo <mckatoo@gmail.com>
2022-05-26 00:17:31 -0300

    Add prisma client

---
+++ src/infra/database/prisma/index.ts
@@ -0,0 +1,17 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation>
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // @ts-ignore
+  if (!global.prisma) {
+    // @ts-ignore
+    global.prisma = new PrismaClient()
+  }
+  // @ts-ignore
+  prisma = global.prisma
+}
+
+export { prisma };
